79b23c759ad87ae10797e15dfdf5958679b40292
Joao Paulo Lana <lana.jpmpl@gmail.com>
2021-11-15 19:21:28 -0300

    Changed controller sintonization (less dependent on derivative term)

--- catkin_ws/src/curve_following/scripts/curve_following.py
+++ catkin_ws/src/curve_following/scripts/curve_following.py
@@ -3,24 +3,46 @@
 import sys
 import rospy
 from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
+from sensor_msgs.msg import *
+from nav_msgs.msg import *
+#from turtlesim.msg import Pose
 from math import *
+from geometry_msgs.msg import PoseStamped
+
+path_pub = rospy.Publisher('/path', Path, queue_size=10)
 
 x0 = 0.0
 y0 = 0.0
-k = 0.2
-d = 1
+k = 5
+kd = 0.01
+d = 0.5
+path = Path()
 
-def pose_callback(data):
-    global x0, y0, theta, k, d
-    x0 = data.x + d * cos(data.theta)
-    y0 = data.y + d * sin(data.theta)
-    theta = data.theta
+#def pose_callback(data):
+#    global x0, y0, theta, k, d
+#    x0 = data.x + d * cos(data.theta)
+#    y0 = data.y + d * sin(data.theta)
+#    theta = data.theta
+
+def odometry_callback(data):
+    global x0, y0, theta, k, d, path
+    theta = 2*asin(data.pose.pose.orientation.z)
+    x0 = data.pose.pose.position.x + d * cos(theta)
+    y0 = data.pose.pose.position.y + d * sin(theta)
+
+    path.header = data.header
+    pose = PoseStamped()
+    pose.header = data.header
+    pose.pose = data.pose.pose
+    path.poses.append(pose)
+    path_pub.publish(path)
 
 def init():
     rospy.init_node('curve_following', anonymous=True)
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
-    rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+    #pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    #rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+    rospy.Subscriber('/odom', Odometry, odometry_callback)
     rate = rospy.Rate(20)
     vel_msg = Twist()
     
@@ -31,18 +53,18 @@ def init():
     
     cx, cy, a = [float(i) for i in [cx, cy, a]]
     t = 0
-    w = 0.4
 
-    while not rospy.is_shutdown():
-        t += 0.05
+    while not rospy.is_shutdown():        
         xf = a*cos(t)*(1-2*pow(sin(t),2))+cx
         yf = a*sin(t)*(1+2*pow(cos(t),2))+cy
-        Vx = k*(xf-x0)-a*sin(t)*(1-2*pow(sin(t),2))-4*a*pow(cos(t),2)*sin(t)
-        Vy = k*(yf-y0)+a*cos(t)*(1+2*pow(cos(t),2))-4*a*pow(sin(t),2)*cos(t)
-        vel_msg.linear.x = Vx
-        vel_msg.linear.y = Vy
-        #vel_msg.linear.x = cos(theta)*Vx+sin(theta)*Vy
-        #vel_msg.angular.z = (-sin(theta)*Vx+cos(theta)*Vy)/d
+        Vx = k*(xf-x0)-kd*a*sin(t)*(1-2*pow(sin(t),2)+4*pow(cos(t),2))
+        Vy = k*(yf-y0)+kd*a*cos(t)*(1+2*pow(cos(t),2)-4*pow(sin(t),2))
+        t += 0.001
+
+        #vel_msg.linear.x = Vx
+        #vel_msg.linear.y = Vy
+        vel_msg.linear.x = cos(theta)*Vx+sin(theta)*Vy
+        vel_msg.angular.z = (-sin(theta)*Vx+cos(theta)*Vy)/d
         rate.sleep()
         pub.publish(vel_msg)
     
